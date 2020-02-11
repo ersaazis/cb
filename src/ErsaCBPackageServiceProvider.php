e47ff1d2621b49282977f6223948c9b000640763
ersaazis <eam24maret@gmail.com>
2020-02-12 01:22:39 +0700

    first commit

---
+++ src/ErsaCBPackageServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ersaazis\CB\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ErsaCBPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+}
