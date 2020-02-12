f91346ec27312411211ba897247a0814c6e919e6
ersaazis <eam24maret@gmail.com>
2020-02-12 10:47:42 +0700

    add plugin

--- src/helpers/Plugin.php
+++ src/helpers/Plugin.php
@@ -6,38 +6,12 @@
  * Time: 11:59 PM
  */
 
-namespace crocodicstudio\crudbooster\helpers;
+namespace ersaazis\cb\helpers;
 
 use Illuminate\Support\Facades\Route;
 
 class Plugin
 {
-
-    public static function has($key)
-    {
-        if(file_exists(app_path("CBPlugins/".$key))) return true;
-        else return false;
-    }
-
-    public static function isNeedUpgrade($pluginKey, $versionToCompare)
-    {
-        $pluginJson = json_decode(file_get_contents(app_path("CBPlugins/".$pluginKey."/plugin.json")), true);
-        if($pluginJson) {
-            return version_compare($pluginJson['version'], $versionToCompare,"!=");
-        }else{
-            return false;
-        }
-    }
-
-    public static function registerDefaultRoute($dir)
-    {
-        Route::group(['middleware' => ['web',\crocodicstudio\crudbooster\middlewares\CBDeveloper::class],
-            'prefix'=>"developer/".getSetting('developer_path'),
-            'namespace' => 'App\CBPlugins\\'.basename(dirname("./../".$dir)).'\Controllers'], function () use ($dir) {
-            cb()->routeController("plugins/".basename(dirname("./../".$dir)),"\App\CBPlugins\\".basename(dirname("./../".$dir))."\Controllers\\".basename(dirname("./../".$dir))."Controller");
-        });
-    }
-
     public function getAllThemes() {
         $plugins_from_user = $this->getAll();
         $plugins_from_master = $this->getAll(__DIR__."/../views/themes");
